69b4bef356ca4259648b4aab03b0526decb62044
17639638825 <562985990@qq.com>
2024-01-30 17:38:50 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+const express = require("express");
+
+const configs = require("./configs");
+const middleware = require("./middleware");
+
+const adminRouter = require("./routes/admin");
+const advRouter = require("./routes/adv");
+const loginRouter = require("./routes/login");
+const realtimehot = require("./routes/realtimehot");
+const zhihuhot = require("./routes/zhihu");
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "upload")));
+app.use(middleware.sendJson());
+app.use(express.json()); // 将请求体中的数据放置到req.body中
+
+// 设置全局响应头
+app.use((req, res, next) => {
+  const val = configs["Access-Control-Allow-Origin"];
+
+  if (typeof val === "string") {
+    res.set("Access-Control-Allow-Origin", val);
+  } else if (val instanceof Array) {
+    const origin = req.headers.origin;
+    if (val.includes(origin)) {
+      res.set("Access-Control-Allow-Origin", origin);
+    }
+  }
+  next();
+});
+// 挂载路由
+app.use(adminRouter);
+app.use(advRouter);
+app.use(loginRouter);
+app.use(realtimehot);
+app.use(zhihuhot);
+//加载静态资源
+app.use("/public", express.static(path.join(__dirname, "./public/")));
+app.use("*", (req, res) => {
+  res.status(404).sendJson("资源不存在", -1);
+});
+
+app.listen(configs.port, configs.bindIp, () => {
+  console.log(`http server is running on ${configs.bindIp}:${configs.port}`);
+});
---
+++ routes/zhihu.js
@@ -0,0 +1,58 @@
+const axios = require("axios");
+const https = require("https");
+const cheerio = require("cheerio");
+const express = require("express");
+const router = express.Router();
+// 引入插件
+const charset = require("superagent-charset");
+const superagent = require("superagent");
+const { resolve } = require("path");
+const { reject } = require("superagent/lib/request-base");
+charset(superagent);
+
+// 登陆接口
+router.get("/zhihu", async (req, res) => {
+  console.log("开始爬虫");
+  //目标地址
+  const url = "https://s.weibo.com/top/summary?cate=realtimehot";
+  //请求头
+  const header = {
+    "User-Agent":
+      "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Mobile Safari/537.36",
+    Host: "s.weibo.com",
+    Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    Cookie: "",
+  };
+  const data = await fetchHTML(url, header);
+  res.json({
+    ok: 1,
+    data: data,
+  });
+});
+//向需要爬虫的地方发送axios请求
+
+async function fetchHTML(r, header) {
+  return new Promise((resolve, reject) => {
+    superagent
+      .get(
+        "https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=50&desktop=true"
+      )
+      .charset()
+      .end(async (err, sres) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(sres.text);
+        }
+      });
+  });
+}
+function extractInformation(html) {
+  // 使用cheerio加载页面内容，类似于jQuery
+  const $ = cheerio.load(html);
+  const content = $("tbody");
+  // console.log(content);
+}
+module.exports = router;
